62bb8e9c7a49e7fa24fcf800cf973f7e7c745e71
nguyentuanphung270301 <nguyentuanphung270301@gmail.com>
2022-10-31 19:26:53 +0700

    first commit

---
+++ setup/main.py
@@ -0,0 +1,41 @@
+import pygame, sys
+from settings import *
+from source.level import Level
+
+class Game:
+    def __init__(self):
+        #general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption('Zelda Game')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+        # main sound
+        main_sound = pygame.mixer.Sound('./assets/audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops = -1)
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type ==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_TAB:
+                        self.level.toggle_menu()
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            self.screen.fill(WATER_COLOR)
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
